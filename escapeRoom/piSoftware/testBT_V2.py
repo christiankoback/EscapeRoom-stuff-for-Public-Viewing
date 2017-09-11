0d2e5a1bf695f6be1ca2689778af4fae62c18a95
chris koback <chris_koback@hotmail.com>
2017-09-10 23:27:47 -0600

    added file for arduino testing-nonblocking sockets

---
+++ escapeRoom/piSoftware/testBT_V2.py
@@ -0,0 +1,28 @@
+import bluetooth
+ 
+#bd_addr = "98:D3:31:FC:59:F0" arduino 1
+bd_addr = "98:D3:31:FC:5A:09" #replace this with different arduino 
+
+port = 1
+sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
+sock.connect((bd_addr,port))
+sock.setblocking(False)
+
+
+data2 = ""
+while 1:
+	
+	data = sock.recv(6)#how many chars at a time
+	if data:
+		data2 = data[1:]
+	print (data2)
+	sock.send(data2)
+    
+	tosend = input()
+	#tosend += "oles\r"
+    
+	if tosend == 'q':
+		break
+    #else:
+    #   sock.send(tosend)  
+sock.close()
