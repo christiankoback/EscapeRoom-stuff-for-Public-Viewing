779ef6d8ab5c3197ad391b3b1e3f1f491ed9b126
RaspberryPi <oles.shnurovskyy@gmail.com>
2017-09-14 21:15:17 -0600

    Added GOOD python BT.

--- escapeRoom/2_piSoftware/testing/bluetoothTest1.py
+++ escapeRoom/2_piSoftware/testing/bluetoothTest1.py
@@ -1,4 +1,5 @@
 import bluetooth
+import time
  
 #bd_addr = "98:D3:31:FC:59:F0" arduino 1
 #bd_addr = "00:06:66:08:5F:A8" #Firefly
@@ -8,17 +9,18 @@ port = 1
 sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
 sock.connect((bd_addr,port))
 print "starting"
-while 1:
-    data = sock.recv(20)#how many chars at a time
-    print data
-    #sock.send(data)
-    
-    tosend = raw_input()
-   # tosend += "oles\r"
-    #sock.send("hello")
-    if tosend != 'q':
-        sock.send(tosend)
-    else:
-        break
 
+while 1:
+
+	tosend = raw_input()
+   	tosend = tosend + "\n"
+	if tosend != "q\n":
+		sock.send(tosend)
+	else:
+		break
+
+
+
+	data = sock.recv(99)  #how many chars at a time
+	print ">" + data + "<"
 sock.close()
