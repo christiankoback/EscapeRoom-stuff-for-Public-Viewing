15f8aa99c03c10f0a8fce8fbbe5645d41bed1467
oles <oles.shnurovskyy@gmail.com>
2017-10-19 18:34:19 -0600

    Laser arduino

--- escapeRoom/1_ArduinoScripts/prod/laserArduino/laserArduino.ino
+++ escapeRoom/1_ArduinoScripts/prod/laserArduino/laserArduino.ino
@@ -135,6 +135,62 @@ void setup() {
   ads1015_4.begin();
 
   setupServoLaser();
+
+
+
+    pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(28, OUTPUT);
+  pinMode(29, OUTPUT);
+  pinMode(30, OUTPUT);
+  pinMode(31, OUTPUT);
+  pinMode(32, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(34, OUTPUT);
+  pinMode(35, OUTPUT);
+  pinMode(36, OUTPUT);
+  pinMode(37, OUTPUT);
+  pinMode(38, OUTPUT);
+  pinMode(39, OUTPUT);
+  pinMode(40, OUTPUT);
+  pinMode(41, OUTPUT);
+  pinMode(42, OUTPUT);
+  pinMode(43, OUTPUT);
+  pinMode(44, OUTPUT);
+  pinMode(45, OUTPUT);
+  pinMode(46, OUTPUT);
+
+
+  
+  digitalWrite(25, HIGH);
+  digitalWrite(26, HIGH);
+  digitalWrite(27, HIGH);
+  digitalWrite(28, HIGH);
+  digitalWrite(29, HIGH);
+  digitalWrite(30, HIGH);
+  digitalWrite(31, HIGH);
+  digitalWrite(32, HIGH);
+  digitalWrite(33, HIGH);
+  digitalWrite(34, HIGH);
+  digitalWrite(35, HIGH);
+  digitalWrite(36, HIGH);
+  digitalWrite(37, HIGH);
+  digitalWrite(38, HIGH);
+  digitalWrite(39, HIGH);
+  digitalWrite(40, HIGH);
+  digitalWrite(41, HIGH);
+  digitalWrite(42, HIGH);
+  digitalWrite(43, HIGH);
+  digitalWrite(44, HIGH);
+  digitalWrite(45, HIGH);
+  digitalWrite(46, HIGH);
+
+
+
+
+
+  
 }
 int codeLen = 3;
 int tempLaser = 0;
