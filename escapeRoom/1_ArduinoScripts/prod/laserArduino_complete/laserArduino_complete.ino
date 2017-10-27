5861c0cd3abfeef2751cbbad7d475d7a797550a6
chris koback <chris_koback@hotmail.com>
2017-10-27 17:30:06 -0600

    added trip laser minimum

--- escapeRoom/1_ArduinoScripts/prod/laserArduino_complete/laserArduino_complete.ino
+++ escapeRoom/1_ArduinoScripts/prod/laserArduino_complete/laserArduino_complete.ino
@@ -32,10 +32,13 @@ int shapeSensorOfInterest = 0;
 int16_t shapeSensorValue = 0;
 
 //valid values for shape sensors
-int cubeMin = 10;
+int cubeMin = 20;
+int cubeMax = 100;
 int triangleMin = 500;
-int tunnelMin = 790;
-int shapeMax = 1000;
+int triangleMax = 600;
+int tunnelMin = 780;
+int tunnelMax = 1000;
+int correctValue = 0;
 
 //keeping track between toggling servo laser & sensor LEDs
 int laserMinutes = 0;
@@ -150,13 +153,15 @@ void toggleLight(int light, int mainSeconds, int mainMinutes, int laserSeconds, 
 // normal laser sensors   (sensor = 0-3 on adc 2,3,4  & 3 on adc1)
 //   - init adafruit adc's
 Adafruit_ADS1015 ads1015_1(0x48);
-//Adafruit_ADS1015 ads1015_2(0x49);     //not needed
-//Adafruit_ADS1015 ads1015_3(0x4A);     //not needed
-Adafruit_ADS1015 ads1015_4(0x4B);
+Adafruit_ADS1015 ads1015_2(0x4B);
+Adafruit_ADS1015 ads1015_3(0x49); 
+Adafruit_ADS1015 ads1015_4(0x4A);     
 
-const int TRIP_LASER_ADC_CHANNEL_AMOUNT = 3;
-int tripLaserMin[TRIP_LASER_ADC_CHANNEL_AMOUNT];
-int tripLaserMax[TRIP_LASER_ADC_CHANNEL_AMOUNT];
+
+const int TRIP_LASER_AMOUNT = 13;
+int adcChannels = 4;
+int tripLaserMin[TRIP_LASER_AMOUNT];
+int tripLaserMax[TRIP_LASER_AMOUNT];
 int tripLaserPercentError = 20;
 
 //variable to keep track of time
@@ -174,20 +179,47 @@ int firstPin_triggerLaser = 30;
 const int AMOUNT_OF_TRIGGER_LASERS = 17;
 
 
-int isGameIncomplete = 2;
+int isGameComplete = 2;
 int isLasersOn = 0;
 int checkTripLasers = 0;
+int laserTripVal = 500;
 int isReset = 0;
+
+int configTimeInSeconds = 5;
+
 // send code to raspberry pi when a sensor is tripped  --- 400 
 //send code to raspberry pi when game is won ---- 402
 // when game is won, turn off shapes & continue reading trip sensors
 
 
+void tripSensorIsTripped(){
+  Serial1.println("400");
+  shapeSensorOfInterest = 0;
+  digitalWrite(triangleShapeSensor   , LOW);
+  digitalWrite(tunnelShapeSensor   , LOW);
+}
 
+void tripLaserConfig(int * laserArrayMin, int * laserArrayMax, int index, int count, int tempSensor){
+  if (count = 0){
+    laserArrayMin[index] = tempSensor;
+    laserArrayMax[index] = tempSensor;
+    count = 1;
+  }
+  //check if sensor value is less than current lowest value, if is then update
+  if ( tempSensor < laserArrayMin[index]){
+    laserArrayMin[index] = tempSensor;
+  }
+  //check if sensor value is greater than current highest value, if is then update
+  if (tempSensor > laserArrayMax[index]){
+    laserArrayMax[index] = tempSensor;
+  }
+}
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
   ads1015_1.begin();
+  ads1015_2.begin();
+  ads1015_3.begin();
   ads1015_4.begin();
   setupServo();
   setupShapeLaser_andSensors();
@@ -198,6 +230,9 @@ void setup() {
   for (int i = firstPin_triggerLaser; i < (firstPin_triggerLaser + AMOUNT_OF_TRIGGER_LASERS); i ++){
      pinMode(i, OUTPUT);
   }
+
+
+  //isReset = 1;              //testing
 }
 
 void loop() {
@@ -208,8 +243,8 @@ void loop() {
     while (inData.length() > 2) {
       token = inData.substring(0,3);
       inData = inData.substring(3,inData.length());
-      Serial.println("token>" + token + "<");
-      Serial.println("inData>" + inData + "<");
+      //Serial.println("token>" + token + "<");
+      //Serial.println("inData>" + inData + "<");
       Serial.println("");
   
       if (token == "000") {                          // Reply to alive request DO NOT DELETE
@@ -218,12 +253,12 @@ void loop() {
      
       } 
       if (token == "401") {                       // start game
-        isGameIncomplete = 0;
+        isGameComplete = 0;
         isLasersOn = 0;
         checkTripLasers = 1;
         shapeSensorOfInterest = 0;
       }else if (token == "999"){                  //reset game
-        isGameIncomplete = 1;
+        isGameComplete = 1;
         isLasersOn = 0;
         checkTripLasers = 0;
         isReset = 1;
@@ -233,7 +268,7 @@ void loop() {
       
     } //end while inData
   } // end if serial available
-  if (isGameIncomplete == 0){         //start game
+  if (isGameComplete == 0){         //start game
     if (isLasersOn == 0){
       Serial.print(" laser is on");
         isLasersOn = 1;
@@ -252,12 +287,12 @@ void loop() {
     int currentTime_seconds = second(currentTime);
     int currentTime_minutes = minute(currentTime);
 
-    Serial.println("before blinking laser");
+    //Serial.println("before blinking laser");
     
     //check if a second has passed, if passed then blink laser
     toggleLight(shapeLaser, currentTime_seconds, currentTime_minutes, laserSeconds, laserMinutes);
 
-    Serial.println("before changing servo laser location");
+    //Serial.println("before changing servo laser location");
 
     //change location of the servo laser
     if( (toggleLaserAmount >= LASER_ON_BEFORE_LASER_LOCATION_CHANGE) && (digitalRead(shapeLaser) == LOW) ){
@@ -290,25 +325,29 @@ void loop() {
     // check shape laser sensors if active
     if (shapeSensorOfInterest == 0){
       shapeSensorValue = ads1015_1.readADC_SingleEnded(0);
-      Serial.println("after checking cube shape sensor");
+      Serial.println("cube sensor: "),Serial.println(shapeSensorValue);
       //check if valid sensor value, if valid then change sensor of interest to next sensor
-      if ( (shapeSensorValue > cubeMin) && (shapeSensorValue < shapeMax) ) {
-         digitalWrite(cubeShapeSensor,LOW);
-         digitalWrite(triangleShapeSensor,HIGH);
-         Serial.println(" ");
-        Serial.println(" ");
-        Serial.println("THIS IS NOT TO BE ON " );
-        Serial.println(" ");
-        Serial.println(" ");
-        
-         shapeSensorOfInterest = 1;
+      if (correctValue == 0){
+        if ( (shapeSensorValue > cubeMin) && (shapeSensorValue < cubeMax) ) {
+          correctValue = 1;
+        }
       }
+      else if ( correctValue == 1){
+        if ( (shapeSensorValue > cubeMin) && (shapeSensorValue < cubeMax) ) {
+          digitalWrite(cubeShapeSensor,LOW);
+          digitalWrite(triangleShapeSensor,HIGH);
+          shapeSensorOfInterest = 1;
+        }
+        else{
+          correctValue = 0;
+        }
+      }else{}        
     }
     else if (shapeSensorOfInterest == 1){
       shapeSensorValue = ads1015_1.readADC_SingleEnded(1);
-  
+      Serial.println("triangle sensor: "),Serial.println(shapeSensorValue);
       //check if valid sensor value, if valid then change sensor of interest to next sensor
-      if ( (shapeSensorValue > triangleMin) && (shapeSensorValue < shapeMax) ){
+      if ( (shapeSensorValue > triangleMin) && (shapeSensorValue < triangleMax) ){
         digitalWrite(triangleShapeSensor,LOW); 
         digitalWrite(tunnelShapeSensor,HIGH);
         shapeSensorOfInterest = 2;
@@ -316,91 +355,20 @@ void loop() {
     }
     else if (shapeSensorOfInterest == 2){
       shapeSensorValue = ads1015_1.readADC_SingleEnded(2);
-  
+      Serial.println("tunnel sensor: "),Serial.println(shapeSensorValue);
       //check if valid sensor value, if valid then change sensor of interest to next sensor
-      if ( (shapeSensorValue > tunnelMin) && (shapeSensorValue < shapeMax) ){
+      if ( (shapeSensorValue > tunnelMin) && (shapeSensorValue < tunnelMax) ){
         //puzzle is COMPLETE
+        isGameComplete = 1;
+        shapeSensorOfInterest = 0;
         Serial1.println("402");
+        digitalWrite(shapeLaser, LOW);
         digitalWrite(tunnelShapeSensor,LOW);
       }
-    }
-    else{}
-  }
-  else{}                              // do nothing
+  }else{}                              // do nothing
   Serial.println("at end of game play");
-
+  }
   if (isReset == 1  ){
-    //turn on lasers
-    if (digitalRead(firstPin_triggerLaser) == LOW){
-      //turn off all trigger lasers -- pin 30-46
-      for (int i = firstPin_triggerLaser; i < (firstPin_triggerLaser + AMOUNT_OF_TRIGGER_LASERS); i ++){
-         digitalWrite(i, HIGH);
-      }
-    }
-    // check trip lasers and log lowest & highest values
-    time_t currentTempTime = now();
-    int tempSeconds = second(currentTempTime);
-    time_t loopTime = now();
-    int tripLaserMinTemp[TRIP_LASER_ADC_CHANNEL_AMOUNT] ={4096, 4096, 4096};
-    int tripLaserMaxTemp[TRIP_LASER_ADC_CHANNEL_AMOUNT] ={0, 0, 0};
-    if (tempSeconds == 59){   //ensure while loop is 2 seconds long, boundry cases -- 59 & 60 minutes of an hour
-      while( second(loopTime) != 1){
-        for (int i = 0; i < TRIP_LASER_ADC_CHANNEL_AMOUNT; i ++){
-          //get sensor value
-          int16_t tempSensor = ads1015_4.readADC_SingleEnded(i);
-          //check if sensor value is less than current lowest value, if is then update
-          if (tempSensor < tripLaserMinTemp[i]){
-            tripLaserMinTemp[i] = tempSensor;
-          }
-          //check if sensor value is greater than current highest value, if is then update
-          else if (tempSensor > tripLaserMinTemp[i]){
-            tripLaserMaxTemp[i] = tempSensor;
-          }
-          else{}
-        }
-        loopTime = now();
-      }
-    }else if (tempSeconds == 60){
-      while( second(loopTime) != 2){
-        for (int i = 0; i < TRIP_LASER_ADC_CHANNEL_AMOUNT; i ++){
-          //get sensor value
-          int16_t tempSensor = ads1015_4.readADC_SingleEnded(i);
-          //check if sensor value is less than current lowest value, if is then update
-          if (tempSensor < tripLaserMinTemp[i]){
-            tripLaserMinTemp[i] = tempSensor;
-          }
-          //check if sensor value is greater than current highest value, if is then update
-          else if (tempSensor > tripLaserMinTemp[i]){
-            tripLaserMaxTemp[i] = tempSensor;
-          }
-          else{}
-        }
-        loopTime = now();
-      }
-    }else{
-      while( ( second(loopTime) - tempSeconds )  <= 2   ){
-        for (int i = 0; i < TRIP_LASER_ADC_CHANNEL_AMOUNT; i ++){
-          //get sensor value
-          int16_t tempSensor = ads1015_4.readADC_SingleEnded(i);
-          //check if sensor value is less than current lowest value, if is then update
-          if (tempSensor < tripLaserMinTemp[i]){
-            tripLaserMinTemp[i] = tempSensor;
-          }
-          //check if sensor value is greater than current highest value, if is then update
-          else if (tempSensor > tripLaserMinTemp[i]){
-            tripLaserMaxTemp[i] = tempSensor;
-          }
-          else{}
-        }
-      }
-      loopTime = now();
-    }
-    
-    //set values to global variables after 2 seconds
-    for (int i = 0; i < TRIP_LASER_ADC_CHANNEL_AMOUNT; i ++){
-      tripLaserMin[i] = tripLaserMinTemp[i] - (int)(tripLaserMinTemp[i]*tripLaserPercentError/100 );
-      tripLaserMax[i] = tripLaserMaxTemp[i] + (int)(tripLaserMaxTemp[i]*tripLaserPercentError/100 );
-    }
     
     //turn off lasers 
     //turn off all shapes
@@ -414,21 +382,39 @@ void loop() {
        digitalWrite(i, LOW);
     }
     isReset = 0;
+    //checkTripLasers = 1;      //testing
+    isGameComplete = 0;
   }
   
   if ( checkTripLasers == 1){
     Serial.println("checking trip sensors");
-    // check all other laser sensors, if any sensors are NOT active then restart the box puzzle
-    for (int i = 0; i < TRIP_LASER_ADC_CHANNEL_AMOUNT; i ++){
-      int16_t tempSensor = ads1015_4.readADC_SingleEnded(i);
-      if (  (tempSensor < tripLaserMin[i]) || (tempSensor > tripLaserMax[i])  ){
-        Serial1.println("400");
-        shapeSensorOfInterest = 0;
-        digitalWrite(triangleShapeSensor   , LOW);
-        digitalWrite(tunnelShapeSensor   , LOW);
+    int tempSensor;
+    
+    //for next 3 adc's, check all 4 channels
+    for (int adcCount = 0; adcCount < (adcChannels - 1); adcCount ++){
+      for (int i = 0; i < adcChannels; i ++){
+        if (adcCount == 0){
+          tempSensor = ads1015_2.readADC_SingleEnded(i);
+          //Serial.print("sensor  "),Serial.print(i),Serial.print(" : "), Serial.println(tempSensor);
+          if (tempSensor < laserTripVal ){
+            tripSensorIsTripped();
+          }
+        }
+        else if (adcCount ==  1){
+          tempSensor = ads1015_3.readADC_SingleEnded(i);
+          if (tempSensor < laserTripVal  ){
+            tripSensorIsTripped();
+          }
+        }
+        else if ( adcCount == 2){
+          tempSensor = ads1015_4.readADC_SingleEnded(i);
+          if (tempSensor < laserTripVal  ){
+            tripSensorIsTripped();
+          }
+        }else{}
       }
+     Serial.println(" ");
     }
-    Serial.println("done checking all trip sensors");
   }
-  Serial.println("done loop");
+  //Serial.println("done loop");
 }
